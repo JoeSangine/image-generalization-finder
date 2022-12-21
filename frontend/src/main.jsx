4ae1dfb02dab3b94f0843b4c0638a2631fbc0436
JoeSangine <joesanginejr@gmail.com>
2022-12-21 16:09:42 -0500

    deployment ready

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Api from './components/Api'
-import Login from './components/Login'
-import Signup from './components/Signup'
+import Login from './components/login'
+import Signup from './components/signup'
 import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
